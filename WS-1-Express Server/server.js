21dc62069575c39c7f52ba2a9abaf7edd43cdb2a
VikrantChauhan <chauhan.vikrant@hotmail.com>
2022-03-15 11:23:06 +0530

    http module

--- WS-1-Express Server/server.js
+++ WS-1-Express Server/server.js
@@ -4,3 +4,18 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
 }).listen(8000);
+
+
+
+// http module example - http://localhost:8000/?year=2017&month=July
+
+
+// var http = require('http');
+// var url = require('url');
+
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   var q = url.parse(req.url, true).query;
+//   var txt = q.year + " " + q.month;
+//   res.end(txt);
+// }).listen(8000);
